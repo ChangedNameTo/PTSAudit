4365606a2518a90978d399a3eca5ec07a02caf29
TheAlpacalypse <william.m95@gmail.com>
2015-10-18 03:19:43 -0400

    Incomplete Plugins
    Currently incomplete python scripts

--- routeDataAnalysis.py
+++ routeDataAnalysis.py
@@ -13,3 +13,17 @@ class routeDataAnalysis:
     ##################
 
     def findClustering(self):
+        #open sql connection
+        cnx = mysql.connector.connect(user = hid_user, password = hid_password, database = 'rambler')
+        cursor = cnx.cursor()
+
+        gmttime = long(time.time())
+        findBorder = gmttime - 120
+
+        query = ("SELECT epochKey, busNumber, nextStop, timeToNextStop FROM rambler "
+            "WHERE epochKey BETWEEN %s AND %s")
+        start_time = gmttime * 1000
+        end_time = findBorder * 1000
+
+        cursor.execute(query, (start_time, end_time))
+        for (epochKey, busNumber, nextStop, timeToNextStop) in cursor:
--- shortenTables.py
+++ shortenTables.py
@@ -4,12 +4,28 @@
 import mysql.connector
 import sys
 import time
-
 from passwords import *
 
 class ShortenTables:
     ##################
     #Global Functions#
     ##################
-    def tableTrim(self, table):
-        currentTime = time.gmtime()
+    def ramblerTableTrim(self):
+        #open sql connection
+        cnx = mysql.connector.connect(user = hid_user, password = hid_password, database = 'rambler')
+        cursor = cnx.cursor()
+
+        gmttime = long(time.time())
+        deletionBorder = gmttime - 3600
+
+        print deletionBorder * 1000
+
+        delete_data = ("DELETE FROM rambler "
+            "WHERE epochKey < %s")
+        cursor.execute(delete_data, (deletionBorder,))
+
+        cnx.commit()
+        cnx.close()
+
+shortenTables = ShortenTables()
+shortenTables.ramblerTableTrim()
